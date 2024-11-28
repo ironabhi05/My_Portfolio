ec043bb85d2d8ef00c8cae27e179ed896043d1b6
Abhinav Mishra <ironabhi728@gmail.com>
2024-11-29 01:41:17 +0530

    Buttons Fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
   return (
     <div id='home' className="Home">
       <section className="pt-10 overflow-hidden bg-blue-100 md:pt-0 sm:pt-16 2xl:pt-16">
-        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="px-4 mx-auto sm:px-2 lg:px-6 max-w-5xl">
           <div className="grid items-center grid-cols-1 md:grid-cols-2">
             <div className="text">
               <h1 className="text-2xl font-bold text-gray-900"> Hey! I Am ABHINAV MISHRA</h1>
@@ -59,8 +59,8 @@ function Home() {
                 </span>
                 <br className="block sm:hidden" />Ask me on <a href="https://www.instagram.com/pt_ironabhi_05/" title="" class="relative transition-all duration-200 text-pink-700 hover:text-pink-700 hover:underline">Instagram</a>
               </p>
-              <button data-aos="zoom-in" className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-tr from-[#ff4694] to-[#776fff] group-hover:from-blue-400 group-hover:to-blue-600 hover:text-white light:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 light:focus:ring-blue-900" style={{ marginTop: '50px' }}>
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white light:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-tr from-[#ff4694] to-[#776fff] group-hover:from-blue-400 group-hover:to-blue-600 hover:text-white light:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 light:focus:ring-blue-900" style={{ marginTop: '10px' }}>
+                <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-white light:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Contact Me
                 </span>
               </button>
